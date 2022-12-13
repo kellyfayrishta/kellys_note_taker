7721dd95c5ae66190698c6e56cf1fb17ba9ed061
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2022-12-13 16:51:07 -0500

    frontend routes created

---
+++ routes/backend.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+
+router.get("/notes", (req, res) => {
+  
+});
+
+router.post("/notes", (req, res) => {
+  
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"));
 
-app.use('/api', apiRoutes)
+// app.use('/api', apiRoutes)
 app.use('/', frontEndRoutes);
 
 
